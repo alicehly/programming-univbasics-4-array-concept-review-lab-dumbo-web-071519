8f474bc95597a2020554ad0be35c879311f6379d
Alice Ly Ms <alicehly@gmail.com>
2019-07-09 02:34:30 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,12 +1,12 @@
 def find_element_index(array, value_to_find)
-# array.length.times { |index|
-#   puts array[index]
+array.length.times { |index|
+  puts array[index]
 end
 
 def find_max_value(array)
-  # array.length.times { |index|
-  #   puts array[index]
-  #   index +=1
+  array.length.times { |index|
+    puts array[index]
+    index +=1
 end
 
 def find_min_value(array)
